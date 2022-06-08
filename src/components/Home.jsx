9189e8872022ee49381862cd2a3fb57728062046
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-06-09 01:47:02 +0200

    Home mostly done

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#3f494e]'>
+        
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-[#b4b4b4]'>Hi! My name is</p>
+            <h1 className='text-4xl sm:text-6xl font-bold text-white'>Christian Karlsson Korbacz</h1>
+            <h2 className='text-3xl sm:text-5xl text-[#b4b4b4]'>I'm a Software/Frontend Developer.</h2>
+            <p className='text-[#b4b4b4] py-4 max-w-[700px]'>I'm a Software and Frontend developer specialising in designing and building 
+            exceptional digital experiences. Currently, I'm focused on building responsive web-applications
+            and small yet fun games. Please take your time to review my work, and read my Resume and cover letter. Or just enjoy the website.</p>
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#292929] hover:border-[#292929]'>View my work
+                <span className='group-hover:rotate-90 duration-300'>
+                <HiArrowNarrowRight className='ml-3' />
+                </span>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
