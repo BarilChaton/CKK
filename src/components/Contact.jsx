433555633b542fb025035d670d8511ed23734473
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-07-20 10:26:22 +0200

    small tweak in contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/7ef2199f-c426-4cd9-a9f2-08b3a0849101" className='flex flex-col max-w-[600px] w-full mb-20'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#141414] text-[#b4b4b4]'>Contact</p>
-                <p className='text-[#b4b4b4] py-4'>// Submit the form below or contact me on my mail - kodman1991@hotmail.com</p>
+                <p className='text-[#b4b4b4] py-4'>// Submit the form below to get in touch.</p>
             </div>
             <input className='p-2 bg-[#7e7e7e] placeholder:text-white rounded-md hover:bg-[#424242] duration-500' type="text" placeholder='Jane Doe' name='name' />
             <input className='my-4 p-2 placeholder:text-white bg-[#7e7e7e] rounded-md hover:bg-[#424242] duration-500' type="email" placeholder='jane@doe.com' name='email' />
